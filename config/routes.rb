bc53ceace2920a9427e30255f026392f2231bf32
Thomas Brus <thomas.brus@me.com>
2012-08-27 17:26:49 +0200

    Added login/logout links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Supermanager::Application.routes.draw do
   resources :contestants
   get "/sign_up" => "contestants#new", as: :sign_up
 
+  # get "/login" => "login_keys#new", as: :login
+  # post "/login/request" => "login_keys#create", as: :request_login
+  # get "/login/verify/:key" => "login_keys#verify", as: :login
+
+  # post "/login" => "sessions#create"
+  # delete "/logout" => "sessions#destroy"
+
   get "/announcements" => "pages#rules", as: :announcements
   get "/players" => "pages#rules", as: :players
   get "/rules" => "pages#rules", as: :rules
