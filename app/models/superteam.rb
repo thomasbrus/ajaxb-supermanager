c8f46dff5cc7739ce36e3eac3275cf8ccc557ec9
Thomas Brus <thomas.brus@me.com>
2012-08-29 20:54:06 +0200

    Association typo

--- app/models/superteam.rb
+++ app/models/superteam.rb
@@ -2,7 +2,7 @@ class Superteam < ActiveRecord::Base
   belongs_to :contestant
   belongs_to :coach
   belongs_to :bonus_player, class_name: "Player"
-  has_many_and_belongs_to :players
+  has_and_belongs_to_many :players
   validates_with Validators::FormationValidator
 end
 
