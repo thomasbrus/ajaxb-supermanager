fd162a78191da4dd53772b1aa8c5c13cf8ea1de1
Thomas Brus <thomas.brus@me.com>
2012-08-29 21:01:06 +0200

    Disabled special validator

--- app/models/superteam.rb
+++ app/models/superteam.rb
@@ -3,6 +3,6 @@ class Superteam < ActiveRecord::Base
   belongs_to :coach
   belongs_to :bonus_player, class_name: "Player"
   has_and_belongs_to_many :players
-  validates_with Validators::FormationValidator
+  #validates_with Validators::FormationValidator
 end
 
--- app/models/validators/formation_validator.rb
+++ app/models/validators/formation_validator.rb
@@ -2,10 +2,10 @@ class Validators::FormationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     #...
   end
-  FORMATION = [
-    GoalKeeper,
-    Defender, Defender, Defender, Defender,
-    Midfielder, Midfielder, Midfielder,
-    Forward, Forward, Forward,
-  ]
+  # FORMATION = [
+  #   GoalKeeper,
+  #   Defender, Defender, Defender, Defender,
+  #   Midfielder, Midfielder, Midfielder,
+  #   Forward, Forward, Forward,
+  # ]
 end
