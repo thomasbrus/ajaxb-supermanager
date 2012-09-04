13a7e6aebdeb1ac04e4ca40b4e06f3a83139d53f
Thomas Brus <thomas.brus@me.com>
2012-09-04 21:30:19 +0200

    Added reply_to

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -9,7 +9,12 @@ class AdminMailer < ActionMailer::Base
       2 => "N.Huntink <nhuntink@planet.nl>"
     }
     @content = message.content
-    mail(to: (receipents[message.receipent.to_i] || receipents[1]), subject: subject, cc: message.email)
+    mail(
+      to: (receipents[message.receipent.to_i] || receipents[1]),
+      subject: subject,
+      cc: message.email,
+      reply_to: message.email
+    )
   end
 
 end
