fd3b520b7242e943543e908f5a8fe1a565486ebc
Thomas Brus <thomas.brus@me.com>
2012-12-27 14:32:36 +0100

    Store secret in env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,13 +1,1 @@
-Supermanager::Application.configure do
-  if ENV['SECRET_TOKEN'].present?
-    config.secret_token = ENV['SECRET_TOKEN']
-  else
-    begin
-      file = Rails.root.to_s + "/config/secret_token"
-      secret_token = open(file).read
-      config.secret_token = secret_token
-    rescue LoadError, Errno::ENOENT => e
-      raise "Secret token couldn't be loaded! Error: #{e}"
-    end
-  end
-end
+Supermanager::Application.config.secret_token = ENV['SECRET_TOKEN']
