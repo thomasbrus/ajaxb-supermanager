2e8ef32452aed0efef32a2621d9c22f62d3a5c97
Thomas Brus <thomas.brus@me.com>
2013-08-02 21:44:05 +0200

    #default_charset= was removed in Rails 3.1

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.default_charset = "utf-8"
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
