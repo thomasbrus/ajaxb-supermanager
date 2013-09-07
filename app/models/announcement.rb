1e46191f25de14be1705a39460e88f64e21c0384
Thomas Brus <thomas.brus@me.com>
2013-09-07 13:53:08 +0200

    Show three announcements instead of two

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,5 +2,5 @@ class Announcement < ActiveRecord::Base
   attr_accessible :content, :title
   belongs_to :author, class_name: 'Contestant'
   validates_presence_of :content, :title, :author
-  scope :latest, order('created_at DESC').limit(2)
+  scope :latest, order('created_at DESC').limit(3)
 end
