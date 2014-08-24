ec169e20284fc78c772e74c1894f70378e012fff
Thomas Brus <thomas.brus@me.com>
2014-08-24 15:55:49 +0200

    Fixed typo in login link emailer

--- app/mailers/login_request_mailer.rb
+++ app/mailers/login_request_mailer.rb
@@ -4,7 +4,7 @@ class LoginRequestMailer < ActionMailer::Base
 
   def login_link(contestant, login_request)
     @contestant, @validation_key = contestant, login_request.validation_key
-    mail(to: contestant.email, subject: "Bevestingslink inloggen bij de Supermanager")
+    mail(to: contestant.email, subject: "Bevestigingslink inloggen bij de Supermanager")
   end
 
 end
