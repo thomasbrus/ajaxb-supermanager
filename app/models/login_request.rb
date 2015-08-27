77d3ecda9f6e8e422da30eae972dae479dd36b91
Thomas Brus <thomas.brus@me.com>
2015-08-27 17:47:04 +0200

    Removed active_attr gem

--- app/models/login_request.rb
+++ app/models/login_request.rb
@@ -2,12 +2,11 @@ class LoginRequest < ActiveRecord::Base
   belongs_to :contestant
   before_create :set_validation_key, :set_expires_at
 
-  private
-    def set_validation_key
-      self.validation_key = SecureRandom.hex.to_s
-    end
+  private def set_validation_key
+    self.validation_key = SecureRandom.hex.to_s
+  end
 
-    def set_expires_at
-      self.expires_at = 24.hours.from_now
-    end
+  private def set_expires_at
+    self.expires_at = 24.hours.from_now
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,10 @@
 class Message
-  include ActiveAttr::Model
+  include ActiveModel::Model
 
-  attribute :name
-  attribute :email
-  attribute :content
-  attribute :receipent
+  attr_accessor :name
+  attr_accessor :email
+  attr_accessor :content
+  attr_accessor :receipent
 
   validates_presence_of :name, :email, :receipent, :content
   validates_length_of :content, maximum: 700
