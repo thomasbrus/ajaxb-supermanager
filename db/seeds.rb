a41b7c036a8e98328928828a7089299b8d29044d
Thomas Brus <thomas.brus@me.com>
2016-08-31 20:56:52 +0200

    Updated season dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# Set up clubs (season 2015 - 2016)
+# Set up clubs (season 2016 - 2017)
 Club.create!(name: 'ADO Den Haag', shorthand: 'ado')
 Club.create!(name: 'AFC Ajax', shorthand: 'aja')
 Club.create!(name: 'AZ', shorthand: 'az')
