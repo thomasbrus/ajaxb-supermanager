1952d7e57a40919c9c31f17f06cc68d45889f666
Thomas Brus <thomas.brus@me.com>
2017-09-01 12:25:36 +0200

    Updated clubs for coming season

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,6 @@
 class Club < ActiveRecord::Base
-  has_many :players
-  has_one :coach
+  has_many :players, dependent: :destroy
+  has_one :coach, dependent: :destroy
   validates_presence_of :name, :shorthand
   validates_uniqueness_of :shorthand
 end
