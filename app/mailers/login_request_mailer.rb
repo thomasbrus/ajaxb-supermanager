4fdc674f7cecda60bf662dbcedec5b0bd154ef22
Thomas Brus <thomas.brus@me.com>
2018-09-13 21:24:01 +0200

    Fixed incorrect email

--- app/mailers/login_request_mailer.rb
+++ app/mailers/login_request_mailer.rb
@@ -1,6 +1,6 @@
 class LoginRequestMailer < ActionMailer::Base
   layout 'email'
-  default from: "Ajax-B Supermanager <ajaxb.supermanager@gmail.com>"
+  default from: "ajaxb.supermanager@gmail.com"
 
   def login_link(contestant, login_request)
     @contestant, @validation_key = contestant, login_request.validation_key
