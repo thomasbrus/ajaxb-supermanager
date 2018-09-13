dbf21e1b8f859d677e62495126fbd217693a8595
Thomas Brus <thomas.brus@me.com>
2018-09-13 21:26:21 +0200

    Really fixed email

--- app/mailers/login_request_mailer.rb
+++ app/mailers/login_request_mailer.rb
@@ -1,6 +1,6 @@
 class LoginRequestMailer < ActionMailer::Base
   layout 'email'
-  default from: "ajaxb.supermanager@gmail.com"
+  default from: "info@thomasbrus.nl"
 
   def login_link(contestant, login_request)
     @contestant, @validation_key = contestant, login_request.validation_key
