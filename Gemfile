512f873ed34b05b9e1de2c7185f54ea9e55af18a
Thomas Brus <thomas@bookingexperts.com>
2022-09-03 12:06:47 +0200

    Fixed some stuff to get it working on new mac

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.5.0'
+ruby '2.7.5'
 
 gem 'rails', '~> 5.0'
 gem 'sass-rails', '~> 5.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@
 Sponsor.create!(name: 'Café Koenders', url: 'http://koenders-zalencentrum.nl', logo: 'koenders.jpg')
 
 # Contestants
-Contestant.create!(name: 'Thomas Brus', email: 'thomas.brus@me.com', is_admin: true)
-Contestant.create!(name: 'Nico Huntink', email: 'nhuntink@caiway.nl', is_admin: true)
+Contestant.create!(name: 'Thomas Brus', email: 'thomas.brus@me.com', is_admin: true, spam_check: 12)
+Contestant.create!(name: 'Nico Huntink', email: 'nhuntink@caiway.nl', is_admin: true, spam_check: 12)
