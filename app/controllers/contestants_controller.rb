a2933250b6ba1df3865f879b5bbb9ce5ac5bd42b
Thomas Brus <thomas@bookingexperts.com>
2023-09-02 12:54:38 +0200

    Enabled registrations
    See 9d63fb0cb4e26b30cf81c86e12c40c4ae0ac03c0

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,5 +1,5 @@
 class ContestantsController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: %i[new create]
   before_action :admin_rights_required, only: [:index]
 
   def index
@@ -11,25 +11,23 @@ class ContestantsController < ApplicationController
   end
 
   def create
-    render(text: "Not allowed", status: :method_not_allowed) and return
+    # render(text: "Not allowed", status: :method_not_allowed) and return
 
     @contestant = Contestant.new(contestant_params)
     @contestant.email = @contestant.email.downcase
 
-    if contestant_params.has_key?(:team_name)
-      @contestant.team = Team.find_or_initialize_by(name: contestant_params[:team_name])
-    end
+    @contestant.team = Team.find_or_initialize_by(name: contestant_params[:team_name]) if contestant_params.has_key?(:team_name)
 
     if @contestant.save
       ContestantMailer.welcome(@contestant, @contestant.login_requests.create).deliver
       redirect_to thank_you_path
     else
-      render action: "new"
+      render action: 'new'
     end
   end
 
   def export
-    send_data Contestant.as_csv, type: "text/csv", filename: "deelnemers.csv", disposition: 'attachment'
+    send_data Contestant.as_csv, type: 'text/csv', filename: 'deelnemers.csv', disposition: 'attachment'
   end
 
   private def contestant_params
